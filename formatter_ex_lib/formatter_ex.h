695fcb94fd74fd72e2218c231d5532b6865d94d8
nin0n <niz6@yandex.ru>
2024-06-06 19:02:35 +0300

    ..

---
+++ formatter_ex_lib/formatter_ex.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+std::ostream& Formatter(std::ostream& out, const std::string& message);
---
+++ hello_world_application/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "formatter_ex.h"
+
+int main()
+{
+    Formatter(std::cout, "hello, world!");
+}
